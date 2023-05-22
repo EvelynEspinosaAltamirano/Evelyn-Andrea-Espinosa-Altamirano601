035e1718bba1918df0e4cfd674bf5405882c66f8
EvelynEspinosaAltamirano <133834355+EvelynEspinosaAltamirano@users.noreply.github.com>
2023-05-22 08:58:44 -0600

    SE SUBIO CORRECTAMENT FUNCION

---
+++ 5 V4TRIFIV/src/funciones/funcionMultiplo.java
@@ -0,0 +1,28 @@
+
+package funciones;
+
+
+public class funcionMultiplo {
+ 
+    public String retornaMultiplo(int num){
+    
+     if(num % 3==0 && num % 5==0){
+           
+            return "TRIFIV";
+    }
+     else if(num==0 ){
+            
+            return "TRI";
+    }
+          else if(num==0 ){
+            
+            return "TRIFIV";
+    }
+        
+        
+            return "Ningun valor";
+    
+    }
+    }
+    
+
