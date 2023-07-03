693093e0c41d622ad1e10879d3210e9d8f80b597
EvelynEspinosaAltamirano <133834355+EvelynEspinosaAltamirano@users.noreply.github.com>
2023-07-03 10:34:43 -0600

    Se agrego correctamente el archivo5

---
+++ 2DA Unidad/Excepciones5/src/excepciones5/Excepciones5.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package excepciones5;
+
+/**
+ *
+ * @author Andrea
+ */
+public class Excepciones5 {
+
+    
+    public static int miMetodo(int a, int b)throws ArithmeticException{
+        
+        if(b == 0){
+        throw new ArithmeticException();
+        
+        }
+        int c=a/b;
+        return c;
+    }
+    
+    public static void main(String[] args){
+        try {
+            int division = miMetodo(10, 0);
+            System.out.println(division);
+        
+        } catch (ArithmeticException e) {
+            System.out.println("Excepcion aritmetica arrojada ");
+            e.printStackTrace();
+        }
+    
+    }
+}
